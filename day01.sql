a46a873e0bef9ce079a7381a932e68e814c089b4
feyza-unal <feyzaunal95@gmail.com>
2023-03-07 22:11:05 +0300

    d01

---
+++ day01.sql
@@ -0,0 +1,11 @@
+CREATE TABLE film
+(
+id int ,
+title VARCHAR(50),
+type VARCHAR(50),
+length int
+);
+INSERT INTO film VALUES (1, 'Kuzuların Sessizliği', 'Korku',130);
+INSERT INTO film VALUES (2, 'Esaretin Bedeli', 'Macera', 125);
+INSERT INTO film VALUES (3, 'Kısa Film', 'Macera',40);
+INSERT INTO film VALUES (4, 'Shrek', 'Animasyon',85);
